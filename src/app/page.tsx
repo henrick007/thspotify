0105dbc7e44460ed83f6496117e93c96cf6e6eaf
henrick007 <gabrielhenriqueferreiraalves07@gmail.com>
2023-12-09 11:31:45 -0400

    Att 09-12-2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Home as Homeicon, Search, Library } from 'lucide-react'
+import { } from 'react'
+import { Home as Homeicon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
 
 export default function Home() {
 
@@ -22,9 +23,30 @@ export default function Home() {
               <Library />
               Your Library</a>
           </nav>
+          <nav className='mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3 '>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Creme</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Esquenta Sertanejo</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Coleção Pagode</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Pisa e Sofre</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Top Brasil</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Funk Hits</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>This is Ferrugem</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Forró Raiz</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Paredão Explode</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>HOT HITS</a>
+          </nav>
         </aside>
         <main className="flex-1 p-6">
-          main
+          <div className='flex items-center gap-4'>
+            <button className='rounded-full bg-black/40  p-2'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/40  p-2'>
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
         </main>
       </div>
       <footer className="bg-zinc-800 border-zinc-700 p-6 ">
